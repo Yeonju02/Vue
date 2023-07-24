33fc637aeb4ca8f0cd768f38e6c5ffc216b0d263
Yeonju02 <cyj32148@naver.com>
2023-07-25 00:05:17 +0900

    Vue

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+
+  <!-- <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>내용</p>
+    </div>
+    <button @click="모달창열렸니 = false">닫기</button>
+  </div> -->
+
+
+  <ModalOpen v-bind:state="모달창열렸니"/>
+
+
+  <div class="menu">
+    <a v-for="i in menus" :key="i"> {{ i }} </a>
+  </div>
+
+
+  <div v-for="i in onerooms" :key="i">
+    <img :src="i.image " class="room-img">
+    <h4 @click="모달창열렸니 = true">{{ i.title }}</h4>
+    <p>{{ i.price }}원</p>
+  </div>
+  
+
+
+</template>
+
+<script>
+
+import data from './assets/oneroom.js';
+import ModalOpen from './components/ModalOpen.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ModalOpen
+  },
+  data() {
+    return {
+      onerooms : data,
+      모달창열렸니 : false,
+      신고수 : [0, 0, 0],
+      menus : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+
+    }
+  }
+}
+</script>
+
+<style>
+
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px ;
+}
+
+.room-img {
+  width: 50%;
+  margin-top: 40px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+
+</style>
---
+++ src/components/ModalOpen.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="black-bg" v-if="state == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>내용</p>
+    </div>
+    <button @click="모달창열렸니 = false">닫기</button>
+    <!-- 여기 모달창열렸니는 왜 state를 쓰면 오류나는지 알아올것(다음시간에 할 거긴함) 또한 모달창 열렸을 때 방마다 내용이 다르게 해야함 -->
+  </div>
+</template>
+
+<script>
+export default {
+    props : ['state']
+}
+
+</script>
+
+<style>
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px ;
+}
+</style>
